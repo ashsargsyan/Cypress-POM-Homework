501339ca9717c1ca8847948f9c9f763123ccab1f
ashsargsyan <ashot.sarg999@gmail.com>
2024-11-28 03:19:58 +0400

    Update with Cypress Mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'n19jg5',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  reporter: 'mochawesome',
 });
